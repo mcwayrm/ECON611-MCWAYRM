6c4823ec07fb8362c798e37fa1824737c52c231b
Ryan McWay <rmcway@dons.usfca.edu>
2019-12-02 18:39:45 -0800

    20. HW 6, Exam Practice, Dec 2 Lecture, HW 5 finished

--- homeworks/HW_5/cross_entropy.py
+++ homeworks/HW_5/cross_entropy.py
@@ -1,56 +1,47 @@
 # Dependencies
 import numpy as np
+import matplotlib.pyplot as plt 
+import scipy
+from scipy import optimize
 
+# SciPy Gradient Optimizer
+def gradient_optimizer(f, x0, eps=.0001, t = .01, num_iter=1000000):
+    for i in range(num_iter):
+        grad = optimize.approx_fprime(x0, f, eps)
+        if all(z < eps for z in grad):
+            x0 = x0 - t * grad
+            return x0 
+        else:
+            x0 = x0 - t * grad
+            grad = optimize.approx_fprime(x0, f, eps)
+            
 # Class
 class Cross_Entropy:
     '''The Cross Entropy Class'''
     def __init__(self, x, y):
-        self.betas = np.zeros(len(x[0]))
-        self.x = np.array(x)
-        self.y = np.eye(len(x))
+        self.x = x
+        self.y = y
+        self.beta = np.zeros((x.shape[1], 1))
     
     def sigmoid(self):
-        '''The Sigmoid form for the log likelihood function'''
-        return 1 / (1 + np.exp(-self.x * self.betas))
+        '''The Sigmoid form for the log likelihood function''' 
+        return 1 / (1 + np.exp(np.dot(-self.x, self.beta)))
     
-    def regression(self):
+    def reg(self):
         '''The Log Likelihood Function'''
-        return -sum((y * np.log(self.sigmoid()) + (1 - y) * np.log(1 - self.sigmoid())))
+        return -sum(y * np.log(self.sigmoid()) + (1 - y) * np.log(1 - self.sigmoid()))
     
     def fit_approx(self):
         '''Make an approximate fit using gradient optimizer'''
-        ## Unsure how to import a py script
-        
-        '''The gradient optimizer from before'''
-        def gradient(f, x0):
-        grad = []
-        for current_x in x0:
-            xplus_eps = current_x + eps
-            xminus_eps = current_x - eps
-            derv = (f(xplus_eps) - f(xminus_eps))/(2 * eps)
-            grad.append(derv)
-        return grad
-        def gradient_optimizer(f, x, eps = 0.0001, t = 0.01, num_iter = 1000):
-        for i in range(num_iter):
-            # Gradient with updated X
-            grad = gradient(f, x)
-            # Check if within acceptable range
-            if all(value < eps for value in grad):
-                return x
-            # Scalar Step
-            t_into_grad = [t * grads for grads in gradient(f, x)]
-            # Subtract each gradient from Xs, update X vector
-            x = [x_old - g for x_old, g in zip(x, t_into_grad)]
-        fitted_approx = gradient_optimizer(regression(), self.betas)
-        
+#         %run gradient_optimizer.py
+        fitted_approx = gradient_optimizer(self.reg(), self.beta)   
         return fitted_approx, x
 
-    def fit(self):
+    def fit(self, x, y, t = 0.01, num_epochs = 1000):
         '''The actual cross gradient provided by Javi'''
-        # A bit unsure how to implement this one
-        beta_2 = zeros
+        beta_2 = np.zeros((x.shape[1], 1))
         for i in num_epochs:
-            p = sigmoid(self.x, beta_2)
-            grad = self.x.T(self.y - p)
+            p = self.sigmoid(x, beta_2)
+            grad = x.T(y - p)
             beta_2 = beta_2 - t * grad
 
--- homeworks/HW_5/gradient_optimizer.py
+++ homeworks/HW_5/gradient_optimizer.py
@@ -1,26 +1,22 @@
-class gradient_optimizer:
-    def __init__(self, f, x):
-        self.f = f
-        self.x = x
-    
-    def gradient(self):
-        '''Calculate derivations of each element in x, store gradient'''
-        grad = []
-        for current_x in self.x:
-            xplus_eps = current_x + eps
-            xminus_eps = current_x - eps
-            derv = (self.f(xplus_eps) - self.f(xminus_eps))/(2 * eps)
-            grad.append(derv)
-        return grad
-    def gradient_optimizer(self, eps = 0.0001, t = 0.01, num_iter = 1000):
-        '''Gradient Optimizer over numerous iterations'''
-        for i in range(num_iter):
-            # Gradient with updated X
-            grad = gradient()
-            # Check if within acceptable range
-            if all(value < eps for value in grad):
-                return self.x
-            # Scalar Step
-            t_into_grad = [t * grads for grads in gradient()]
-            # Subtract each gradient from Xs, update X vector
-            self.x = [x_old - g for x_old, g in zip(self.x, t_into_grad)]
+     
+def gradient_optimizer(f, x, eps = 0.0001, t = 0.01, num_iter = 1000):
+    def gradient(f, x0):
+    '''Calculate derivations of each element in x, store gradient'''
+    grad = []
+    for current_x in x0:
+        xplus_eps = current_x + eps
+        xminus_eps = current_x - eps
+        derv = (f(xplus_eps) - f(xminus_eps))/(2 * eps)
+        grad.append(derv)
+    return grad
+    '''Gradient Optimizer over numerous iterations'''
+    for i in range(num_iter):
+        # Gradient with updated X
+        grad = gradient(f, x)
+        # Check if within acceptable range
+        if all(value < eps for value in grad):
+            return x
+        # Scalar Step
+        t_into_grad = [t * grads for grads in gradient(f, x)]
+        # Subtract each gradient from Xs, update X vector
+        x = [x_old - g for x_old, g in zip(x, t_into_grad)]
